21255a5860f7387f69d77d6fe22706177fc22bf0
hkjs96 <hkjs96@naver.com>
2022-12-03 00:09:07 +0900

    develop init

--- src/main/java/com/sideproject/service/EmailServiceImpl.java
+++ src/main/java/com/sideproject/service/EmailServiceImpl.java
@@ -2,6 +2,7 @@ package com.sideproject.service;
 
 import com.sideproject.util.RedisUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
