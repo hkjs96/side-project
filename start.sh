7dbd59196b5ae445c4693802addc131b5da7a484
hkjs96 <hkjs96@naver.com>
2022-12-12 18:46:27 +0900

    쉘스크립트 다시 적용

--- start.sh
+++ start.sh
@@ -3,23 +3,26 @@
 if [ -n "$1" ]
 then
   JARFILE=`find . -type f -iname "$1"`
-  echo ${JARFILE}
+  echo JAR_FILE_PATH: ${JARFILE}
 
   if [ -e ${JARFILE} ]
   then
+      echo pgrep -f ${JARFILE}: `pgrep -f ${JARFILE}`
       kill -15 `pgrep -f ${JARFILE}`
   elif [ ! -e ${JARFILE} ]
   then
       echo "File not exists."
   fi
 
+  echo clean
   ./gradlew clean
+  echo build
   ./gradlew build
 
+  echo docker-compose redis
   docker-compose -f docker-compose/redis/docker-compose.yml up -d
 
-  pwd
-
+  echo "nohup java -jar `find . -type f -iname ${JARFILE}` &"
   nohup java -jar `find . -type f -iname ${JARFILE}` &
 else
   echo "파라미터가 필요"
