cd3ee38dd7c5a3ef80e59a0da270e6ed435a112e
hkjs96 <hkjs96@naver.com>
2022-12-12 17:40:48 +0900

    build clean 과정 용 쉘 스크립트

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ -e "$1" ]
+then
+    kill -15 $(pgrep -f "$1")
+elif [ ! -e "$1" ]
+then
+    echo "File not exists."
+else
+    echo "파라미터가 필요"
+fi
+
+./gradlew clean
+./gradlew build
+
+docker-compose -f docker-compose/redis/docker-compose.yml up -d
+
+nohup java -jar $(find . -type f -iname "$1") 1>/dev/null 2>&1 &
